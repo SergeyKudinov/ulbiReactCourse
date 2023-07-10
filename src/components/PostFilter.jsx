82550c75bd59d03003f0384dedf8670e740a3930
SergeyKudinov <serezha.kudinov.1995@mail.ru>
2023-07-10 11:02:36 +0300

    add filter for posts

---
+++ src/components/PostFilter.jsx
@@ -0,0 +1,23 @@
+import MyInput from './UI/input/MyInput'
+import MySelect from './UI/select/MySelect'
+
+export default function PostFilter({filter, setFilter}) {
+    return (
+    <div>
+        <MyInput
+            value={filter.query}
+            onChange={e => setFilter({...filter, query: e.target.value})}
+            placeholder="Поиск..."
+        />
+        <MySelect
+            defaultValue='Сортировка'
+            value={filter.sort}
+            onChange={selectedSort => setFilter({...filter, sort: selectedSort})}
+            options={[
+                {value: 'title', name: 'По названию'},
+                {value: 'body', name: 'По описанию'}
+            ]}
+        />
+        </div>
+    )
+}
