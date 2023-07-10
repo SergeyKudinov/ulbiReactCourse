9324798b2d6824aab535c53b58f86e051ac47e76
SergeyKudinov <serezha.kudinov.1995@mail.ru>
2023-07-10 11:37:45 +0300

    add animation with react-transition-group

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,3 +1,4 @@
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import PostItem from './PostItem'
 
 export default function PostList({props, title, remove}) {
@@ -13,9 +14,17 @@ export default function PostList({props, title, remove}) {
             <h1>
                 {title}
             </h1>
-            {props.map((post, index) =>
-                <PostItem remove={remove} number={index + 1} post={post} key={post.id} />
-            )}
+            <TransitionGroup>
+                {props.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                        <PostItem remove={remove} number={index + 1} post={post} />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </>
     )
 }
